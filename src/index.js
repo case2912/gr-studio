23f25b4343c5b636f89e38fd64aa2d212212d40d
case2912 <shiroyama@maia.eonet.ne.jp>
2017-02-08 18:02:26 +0900

    modify; editor theme

--- src/index.js
+++ src/index.js
@@ -1,26 +1,2 @@
-import gr from 'grimoirejs';
-import MaterialFactory from "grimoirejs-fundamental/ref/Material/MaterialFactory";
-import txt from '../index.sort';
-import uuid from 'node-uuid';
-import ace from 'brace';
-import {
-    watch
-} from 'watchjs';
-// require('brace/theme/monokai');
-require('./monokai');
-
-var obj = {
-    text: txt
-}
-
-var editor = ace.edit('javascript-editor');
-editor.setTheme('ace/theme/monokai');
-editor.getSession().setValue(txt);
-setInterval(() => {
-    obj.text = editor.getValue();
-}, 3000);
-watch(obj, "text", () => {
-    const id = uuid.v4();
-    MaterialFactory.addSORTMaterial(id, obj.text);
-    gr("#main")("render-quad").setAttribute('material', 'new(' + id + ')');
-});
+import render from './render'
+window.addEventListener("load", render(), false);
---
+++ src/render.js
@@ -0,0 +1,46 @@
+import "./regenerator";
+import "grimoirejs/register";
+import "grimoirejs-math/register";
+import "grimoirejs-fundamental/register";
+import gr from 'grimoirejs';
+import MaterialFactory from 'grimoirejs-fundamental/ref/Material/MaterialFactory';
+import Parser from 'grimoirejs-fundamental/ref/Sort/Parser';
+import Material from 'grimoirejs-fundamental/ref/Material/Material';
+import text from '../index.sort';
+import uuid from 'node-uuid';
+import ace from 'brace';
+import {
+    watch
+} from 'watchjs';
+import 'brace/mode/glsl';
+import './theme';
+
+const render = () => {
+    const sort = {
+        text: text
+    }
+    const editor = ace.edit('sort-editor');
+    editor.getSession().setMode('ace/mode/glsl');
+    editor.setTheme('ace/theme/grimoire-sort');
+    editor.setOptions({
+        fontFamily: "ricty",
+        fontSize: "13pt"
+    });
+    editor.getSession().setValue(sort.text);
+    setInterval(() => {
+        sort.text = editor.getValue();
+    }, 3000);
+    let lastId = 'initial-material';
+    watch(sort, "text", async() => {
+        const id = uuid.v4();
+        // try {
+        const obj = await Parser.parse(sort.text);
+        const gl = gr("#main").rootNodes[0].companion.get('gl');
+        const material = new Material(gl, obj);
+        gr("#main")("render-quad").setAttribute('material', material);
+        // } catch (e) {
+        //     console.log(1);
+        // } finally {}
+    });
+}
+export default render;
